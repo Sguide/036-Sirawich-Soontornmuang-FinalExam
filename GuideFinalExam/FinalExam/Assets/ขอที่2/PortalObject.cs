2fbc8dda7dee1c56179ff406afe09c7f4cb8f84b
SsGuide <sgiguide@gmail.com>
2020-06-19 16:11:17 +0700

    036_Sirawich_FinalExam

---
+++ GuideFinalExam/FinalExam/Assets/ขอที่2/PortalObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalObject : MonoBehaviour
+{
+    public GameObject[] PortalDoor;
+    bool Switch = true;
+    Vector3 BlueDoor;
+    Vector3 OrangeDoor;
+
+    private void Start()
+    {
+        BlueDoor = new Vector3(PortalDoor[0].transform.position.x, PortalDoor[0].transform.position.y, PortalDoor[0].transform.position.z-0.1f);
+        OrangeDoor = new Vector3(PortalDoor[1].transform.position.x, PortalDoor[1].transform.position.y, PortalDoor[1].transform.position.z-0.1f);
+    }
+
+    private void Update()
+    {
+        if(Switch)
+        {
+            transform.LookAt(transform.position);
+            transform.Translate(Vector3.forward * 0.01f * Time.deltaTime);
+        }
+        else
+        {
+            transform.LookAt(transform.position);
+            transform.Translate(Vector3.forward * 0.01f * Time.deltaTime);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(Switch)
+        {
+            if (collision.gameObject.tag == "Blue")
+            {
+                transform.position = transform.position;
+                Switch = !Switch;
+                Debug.Log("Hello");
+            }
+        }
+        else
+        {
+            if(collision.gameObject.tag == "Orange")
+            {
+                transform.position = transform.position;
+                Switch = !Switch;
+            }
+        }
+    }
+}
---
+++ GuideFinalExam/FinalExam/Assets/ข้อที่1/Map.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject[] BlockMap;
+
+    private void Start()
+    {
+        for (int y = 0; y < 15; y++)
+        {
+            for (int z = 0; z < 15; z++)
+            {
+                for (int x = 0; x < 15; x++)
+                {
+                    transform.position = new Vector3(x, y, z);
+                    Instantiate(BlockMap[Random.Range(0, BlockMap.Length)], transform.position, Quaternion.identity);         
+                }
+                transform.position += new Vector3(0f, 0f, z);
+            }
+            transform.position += new Vector3(0f, 0f, 0f); 
+        }
+    }
+}
+
+// Hint
+// Random.Range
